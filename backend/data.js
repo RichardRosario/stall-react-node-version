adee8e655f82f2702ddd4d6807d43bd11bfe26cf
Richard Rosario <tech2js@gmail.com>
2020-07-20 10:41:02 +0800

    setting up node backend and install dev dependencies

---
+++ backend/data.js
@@ -0,0 +1,52 @@
+export default {
+  products: [
+    {
+      id: "1",
+      name: "Slim shirt",
+      category: "shirt",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "nike",
+      rating: 4.5,
+      numReviews: 10,
+      desc:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis, officiis!",
+    },
+    {
+      id: "2",
+      name: "long sleeve shirt",
+      category: "shirt",
+      image: "/images/d1.jpg",
+      price: 64,
+      brand: "nike",
+      rating: 4.5,
+      numReviews: 10,
+      desc:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis, officiis!",
+    },
+    {
+      id: "0",
+      name: "t-shirt",
+      category: "shirt",
+      image: "/images/d1.jpg",
+      price: 45,
+      brand: "under armour",
+      rating: 4.5,
+      numReviews: 10,
+      desc:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis, officiis!",
+    },
+    {
+      id: "3",
+      name: "Slim pant",
+      category: "pants",
+      image: "/images/d1.jpg",
+      price: 65,
+      brand: "jag",
+      rating: 4.5,
+      numReviews: 10,
+      desc:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis, officiis!",
+    },
+  ],
+};
