70a5f9a13a4fcbfa9399e1ca63cd05c79a9cca3a
Richard Rosario <tech2js@gmail.com>
2020-07-21 13:48:31 +0800

    add reducers connect it to store

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,13 @@ app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
+app.get("/api/products/:id", (req, res) => {
+  const productId = req.params.id;
+  const product = data.products.find((x) => x.id === productId);
+  if (product) res.send(product);
+  else res.status(404).send({ msg: "Product not found." });
+});
+
 app.listen(5000, () => {
   console.log("Server start at http://localhost:5000");
 });
--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,10 +1,21 @@
-import React from "react";
-import data from "../data";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { detailsProduct } from "../actions/productActions";
 
 function ProductScreen(props) {
-  let prodId = props.match.params.id;
-  const product = data.products.find(({ id }) => id === prodId);
+  // let prodId = props.match.params.id;
+  // const product = data.products.find(({ id }) => id === prodId);
+  const productDetails = useSelector((state) => state.productDetails);
+  const { product, loading, error } = productDetails;
+  const dispatch = useDispatch();
+  const prodId = props.match.params.id;
+  useEffect(() => {
+    dispatch(detailsProduct(prodId));
+    return () => {
+      // cleanup
+    };
+  }, [dispatch, prodId]);
   // console.log(`this is ${prodId}`);
   // console.log(product);
   return (
@@ -14,46 +25,53 @@ function ProductScreen(props) {
           Back to home
         </Link>
       </div>
-      <div className="details">
-        <div className="details-image">
-          <img src={product.image} alt="product" />
-        </div>
+      {/* check loading */}
+      {loading ? (
+        <div>loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <div className="details">
+          <div className="details-image">
+            <img src={product.image} alt="product" />
+          </div>
 
-        <div className="details-info">
-          <ul>
-            <li>
-              <h4>{product.name}</h4>
-            </li>
-            <li>
-              {product.rating} Stars ({product.numReviews} Reviews)
-            </li>
-            <li>${product.price}</li>
-            <li>
-              Description:
-              {product.desc}
-            </li>
-          </ul>
+          <div className="details-info">
+            <ul>
+              <li>
+                <h4>{product.name}</h4>
+              </li>
+              <li>
+                {product.rating} Stars ({product.numReviews} Reviews)
+              </li>
+              <li>${product.price}</li>
+              <li>
+                Description:
+                {product.desc}
+              </li>
+            </ul>
+          </div>
+          <div className="details-action">
+            <ul>
+              <li>Price: ${product.price}</li>
+              <li className="status">Status: {product.status}</li>
+              <li>
+                Qty:
+                <select>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+              </li>
+              <li>
+                <button className="btn">Add to cart</button>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="details-action">
-          <ul>
-            <li>Price: ${product.price}</li>
-            <li className="status">Status: {product.status}</li>
-            <li>
-              Qty:
-              <select>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-              </select>
-            </li>
-            <li>
-              <button className="btn">Add to cart</button>
-            </li>
-          </ul>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
