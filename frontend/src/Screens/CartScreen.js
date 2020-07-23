a6bca43bcc6f168ff8d69b35c7dc5f3be3f1cf87
Richard Rosario <tech2js@gmail.com>
2020-07-23 09:37:16 +0800

    added add to cart action

--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../actions/cartActions";
 
 function CartScreen(props) {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   const productId = props.match.params.id;
   const dispatch = useDispatch();
   const qty = props.location.search
@@ -15,7 +17,52 @@ function CartScreen(props) {
     }
   }, [dispatch, productId, qty]);
 
-  return <div>this is the Cart Page</div>;
+  return (
+    <div className="cart">
+      <div className="cart-list">
+        <ul className="cart-list-contaier">
+          <li>
+            <h3>Shopping Cart</h3>
+            <div>Price</div>
+          </li>
+          {cartItems.length === 0 ? (
+            <div>Cart is empty</div>
+          ) : (
+            cartItems.map((item) => (
+              <div key={item.qty}>
+                <img src={item.image} alt="product" />
+                <div className="cart-name">
+                  <div>{item.name}</div>
+                  <div>
+                    Qty:
+                    <select name="" id="">
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                    </select>
+                  </div>
+                </div>
+                <div>{item.price}</div>
+              </div>
+            ))
+          )}
+        </ul>
+      </div>
+      <div className="cart-action">
+        <h3>
+          Subtotal ({cartItems.reduce((a, c) => a + c.qty, 0)} items) : $
+          {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+        </h3>
+        <button
+          className="btn primary"
+          disabed={(cartItems.length === 0).toString()}
+        >
+          Proceed to checkout
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default CartScreen;
